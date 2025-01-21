4cdf322bc8d1288c406189c71086076dd962563f
Rafael Medeiros <pedro.mape7@gmail.com>
2025-01-20 23:31:34 -0300

    update routes and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Minha primeira API com fast API
+
+Esta é minha primeira API feita com o Fast API, então tomei a liberdade de experimentar algumas coisas, como todas os 4 principais métodos http (GET, POST, PUT e DELETE), mas por ser apenas um teste, apenas modifica um dicionario presente no próprio código, sendo assim, todas as mudanças são perdidas quando o código é rodado novamente.
+
+## Para rodar o código
+caso queira rodar o código em sua máquina, recomendo a criação de um ambiente virtual (venv), após isso, baixe as dependências do projeto listadas no arquivo requirements.txt rodando o seuinte comando no terminal: <br>
+
+`pip install -r requirements.txt`
+
+Para rodar o servidor, digite no terminal o comando: <br>
+
+`uvicorn main:app`
+
+e pronto.
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Vendas(BaseModel):
+    item: str
+    preco_unitario: int
+    quantidade: int
+
 vendas = {
-    1: {'item': 'lata', 'preco_unitario': 4, 'quantidades': 5},
+    1: {'item': 'lata', 'preco_unitario': 4, 'quantidade': 5},
     2: {'item': 'garrafa 2L', 'preco_unitario': 15, 'quantidade': 5},
     3: {'item': 'garrafa 750ml', 'preco_unitario': 10, 'quantidade': 5},
     4: {'item': 'lata mini', 'preco_unitario': 2, 'quantidade': 5}
@@ -13,9 +20,37 @@ vendas = {
 def home():
     return {'vendas': len(vendas)}
 
-@app.get('/vendas/{id_venda}')
+@app.get('/vendas')
+def get_all_vendas():
+    return vendas
+
+@app.get('/venda/{id_venda}')
 def get_venda(id_venda: int):
     if id_venda in vendas:
         return vendas[id_venda]
     else:
-        return {'erro': 'ID invalido'}
+        return JSONResponse(status_code=400, content={'erro': 'ID invalido'})
+
+@app.post('/vender')
+def post_venda(venda: Vendas):
+    data_venda = venda.__dict__
+    n = list(vendas.keys())[-1] + 1
+    vendas[n] = data_venda
+    return JSONResponse(status_code=201, content={'create': vendas[n], 'id': n})
+
+@app.delete('/delete/{id_venda}')
+def delete_venda(id_venda: int):
+    if id_venda in vendas:
+        del vendas[id_venda]
+        return JSONResponse(status_code=204, content={})
+    else:
+        return JSONResponse(status_code=404, content={'Error': 'ID not fund'})
+
+@app.put('/atualizar/{id_venda}')
+def put_venda(id_venda: int, venda: Vendas):
+    if id_venda in vendas:
+        data_venda = venda.__dict__
+        vendas[id_venda] |= data_venda
+        return JSONResponse(status_code=200, content=vendas[id_venda])
+    else:
+        return JSONResponse(status_code=404, content={'Error': 'ID not fund'})
