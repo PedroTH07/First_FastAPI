9137684f99c09163f7e79d543deec3ace46f4ac4
Rafael Medeiros <pedro.mape7@gmail.com>
2025-01-21 22:12:49 -0300

    now the api can POST and GET images

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse, FileResponse
 from pydantic import BaseModel
+from pathlib import Path
+import shutil
 
 app = FastAPI()
+
+UPLOAD_FOLDER = Path('uploads')
+UPLOAD_FOLDER.mkdir(exist_ok=True)
 
 class Vendas(BaseModel):
     item: str
@@ -54,4 +59,21 @@ def put_venda(id_venda: int, venda: Vendas):
         return JSONResponse(status_code=200, content=vendas[id_venda])
     else:
         return JSONResponse(status_code=404, content={'Error': 'ID not fund'})
+
+@app.post('/postimage/')
+def post_image_venda(file: UploadFile = File(...)):
+    if file.content_type not in ['image/jpeg', 'image/png']:
+        return JSONResponse(status_code=400, content={'Error': 'tipo de arquivo não suportado'})
+    
+    file_path = UPLOAD_FOLDER / file.filename
+    with file_path.open('wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return JSONResponse(status_code=201, content={'file name': file.filename, 'message': 'Imagem criada'})
+
+@app.get('/image/{image_name}')
+def get_image_venda(image_name: str):
+    file_path = UPLOAD_FOLDER / image_name
+    if not file_path.exists():
+        return JSONResponse(status_code=400, content={'Error': 'Imagem não encontrada'})
+    return FileResponse(file_path)
 
